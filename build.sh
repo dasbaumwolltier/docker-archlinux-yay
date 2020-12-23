72c30c729b3d9e8ff4a361cc7189890fcb8fc320
Gabriel Guldner <gabriel@guldner.eu>
2020-12-23 22:44:58 +0100

    Debug

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -x
+
 IMAGE_NAME="archlinux-yay"
 VERSION="latest"
 
@@ -10,7 +12,9 @@ docker build . > $TEMPFILE
 
 IMAGE_ID="$(cat "$TEMPFILE" | tail -1 | cut -d' ' -f3 | tr ' ' '\n')"
 
+set +x
 echo $HUB_PASS | docker login --username "$HUB_USER" --password-stdin
+set -x
 
 docker tag "$IMAGE_ID" "dasbaumwolltier/$IMAGE_NAME:$VERSION"
 docker push "dasbaumwolltier/$IMAGE_NAME:$VERSION"
