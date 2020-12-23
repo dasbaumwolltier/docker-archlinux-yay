475758603d69aa3a93a9d069d49e0d8e8221fb8e
Gabriel Guldner <gabriel@guldner.eu>
2020-12-23 22:36:49 +0100

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+IMAGE_NAME="archlinux-yay"
+VERSION="latest"
+
+TEMPFILE="$(mktemp)"
+
+docker pull archlinux
+docker build . > $TEMPFILE
+
+IMAGE_ID="$(cat "$TEMPFILE" | tail -1 | cut -d' ' -f3 | tr ' ' '\n')"
+
+echo $HUB_PASS | docker login --username "$HUB_USER" --password-stdin
+
+docker tag "$IMAGE_ID" "dasbaumwolltier/$IMAGE_NAME:$VERSION"
+docker push "dasbaumwolltier/$IMAGE_NAME:$VERSION"
+
+if [ $VERSION != "latest" ]; then
+    docker tag "$IMAGE_ID" "dasbaumwolltier/$IMAGE_NAME:latest"
+    docker push "dasbaumwolltier/$IMAGE_NAME:latest"
+fi
+
+docker logout 
